2bd0f3453b7a575b9681ea71ee57fa447a2aa07a
Inesh Loka <56318873+inesh-l@users.noreply.github.com>
2024-01-10 10:15:18 -0500

    updates

--- app/components/nav/navbar.js
+++ app/components/nav/navbar.js
@@ -43,11 +43,6 @@ export function Navbar(props) {
                         </a>
                     </li>
                     <li>
-                        <a href="blog" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
-                            Blog
-                        </a>
-                    </li>
-                    <li>
                         <a href="resume" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">
                             Resume
                         </a>
--- app/projects/page.js
+++ app/projects/page.js
@@ -7,25 +7,10 @@ export default function Home() {
       <Navbar currentPage="Projects" />
 
       <div className="flex flex-col items-center">
-        <a href="https://github.com/inesh-l/stickerbomb" target='_blank' className="min-w-[50%]">
-          <ProjectCard 
-          name="Stickerbomb"
-          description="An application to isolate subjects of images and turn them into cartoon stickers" />
-        </a>
-        <a href="https://github.com/inesh-l/soundbite" target='_blank' className="min-w-[50%]">
-          <ProjectCard
-          name="Soundbite"
-          description="A lightweight utility to capture and modify sound clips from your computer" />
-        </a>
         <a href="" target='_blank' className="min-w-[50%]">
           <ProjectCard
           name="AI Club Project Series"
           description="A collection of AI model implementations to teach club members the fundamentals of deep learning" />
-        </a>
-        <a href="https://github.com/inesh-l/vscode" target='_blank' className="min-w-[50%]">
-          <ProjectCard
-          name="VSCode - Tauri"
-          description="An in-progress migration of Code - OSS (Visual Studio Code) to Tauri" />
         </a>
         <a href="https://github.com/inesh-l/MazeSolver" target='_blank' className="min-w-[50%]">
           <ProjectCard
